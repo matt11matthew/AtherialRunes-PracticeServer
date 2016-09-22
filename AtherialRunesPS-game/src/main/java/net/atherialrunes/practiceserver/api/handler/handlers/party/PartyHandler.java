71b47ce7c16cb9285dfe7c0f4fedf6b022c4fc95
Matthew <matt11matthew@gmail.com>
2016-09-21 23:55:32 -0400

    Registered p command

--- AtherialRunesPS-game/src/main/java/net/atherialrunes/practiceserver/api/handler/handlers/party/PartyHandler.java
+++ AtherialRunesPS-game/src/main/java/net/atherialrunes/practiceserver/api/handler/handlers/party/PartyHandler.java
@@ -3,6 +3,7 @@ package net.atherialrunes.practiceserver.api.handler.handlers.party;
 import net.atherialrunes.practiceserver.GameAPI;
 import net.atherialrunes.practiceserver.api.command.AtherialCommandManager;
 import net.atherialrunes.practiceserver.api.handler.ListenerHandler;
+import net.atherialrunes.practiceserver.api.handler.handlers.party.commands.CommandP;
 import net.atherialrunes.practiceserver.api.handler.handlers.party.commands.CommandPQuit;
 import net.atherialrunes.practiceserver.api.handler.handlers.party.commands.CommandParty;
 import net.atherialrunes.practiceserver.api.player.GamePlayer;
@@ -17,6 +18,7 @@ public class PartyHandler extends ListenerHandler {
         AtherialCommandManager cm = new AtherialCommandManager();
         cm.registerCommand(new CommandParty("party"));
         cm.registerCommand(new CommandPQuit("pquit"));
+        cm.registerCommand(new CommandP("p"));
     }
 
     @Override
