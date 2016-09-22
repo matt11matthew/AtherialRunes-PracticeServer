25d8db84cc7766ac1c7da6cf5c1a617b2364d033
Matthew <matt11matthew@gmail.com>
2016-09-22 17:27:16 -0400

    Food Vendor

--- AtherialRunesPS-game/src/main/java/net/atherialrunes/practiceserver/PracticeServer.java
+++ AtherialRunesPS-game/src/main/java/net/atherialrunes/practiceserver/PracticeServer.java
@@ -15,6 +15,7 @@ import net.atherialrunes.practiceserver.api.handler.handlers.spawner.commands.Co
 import net.atherialrunes.practiceserver.api.handler.handlers.spawner.commands.CommandShowMs;
 import net.atherialrunes.practiceserver.api.handler.handlers.health.HealthHandler;
 import net.atherialrunes.practiceserver.api.handler.handlers.vendor.VendorHandler;
+import net.atherialrunes.practiceserver.api.handler.handlers.vendor.vendors.foodvendor.FoodVendor;
 import net.atherialrunes.practiceserver.api.handler.handlers.vendor.vendors.itemvendor.ItemVendor;
 import net.atherialrunes.practiceserver.api.handler.handlers.vendor.vendors.misc.Healer;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -49,6 +50,7 @@ public class PracticeServer extends JavaPlugin {
     private void registerVendors() {
         VendorHandler.registerVendor(new ItemVendor("Item Vendor", 9));
         VendorHandler.registerVendor(new Healer("Healer"));
+        VendorHandler.registerVendor(new FoodVendor("Food Vendor", 18));
     }
 
     private void registerCommands() {
